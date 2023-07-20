62bbcac8080a1ccd150b7825b433b159167ca6c4
Mohamed Amr <momo.amr.f@gmail.com>
2023-07-20 16:14:20 +0300

    added matplotlib graph instead of cmd output

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import math
 import ast
-
+import matplotlib.pyplot as plt
+import numpy as np
 from helpers.validation import *
 
 def F(X):
+    """Takes X and returns F(x)
+
+    Args:
+        X (float): input value to the equation (x)
+
+    Returns:
+        float: Output of the function after evaluation (F(x))
+    """
     x = X
     return eval(equation)
 
@@ -21,8 +30,20 @@ r = int(input())+1
 validate_min_max(l, r)
 
 
-# print(equation)
+xpoints = np.arange(l,r)
+ypoints = np.zeros(r-l)
+
+print(int(xpoints.size))
+print(int(ypoints.size))
+
 for i in range(l, r):
-    Y = F(i)
-    print("@ X = " + str(i) + " Y = " + str(Y));
+    ypoints[i-l] = F(i)
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
+# plot
+# for i in range(l, r):
+#     Y = F(i)
+#     print("@ X = " + str(i) + " Y = " + str(Y));
 
