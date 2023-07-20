658bbd3ff943e298fdc3b5b361ab6a0fe86f33f3
Mohamed Amr <momo.amr.f@gmail.com>
2023-07-20 15:16:37 +0300

    added input validation feature

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
 import math
-equation = input("Enter the Equation to be solved: ")
-equation = equation.replace("^", "**")
+import ast
 
+def validate_min_max(l, r):
+    while r <= l:
+        print("INVALID: The maximum must be greater than minimum")
+        l = int(input("Enter Minimum: "))
+        r = int(input("Enter Maximum: "))+1
+
+def valid_character(c):
+    validCharacters = "x+-*/^ 0123456789()"
+    if validCharacters.find(c) == -1:
+        return False
+    else:
+        return True
+
+def input_validation(input_equation):
+    # Check for python syntax errors
+    try:
+        ast.parse(input_equation)
+    except SyntaxError:
+        return False
+
+    # Check for valid input characters
+    for c in input_equation:
+        if not valid_character(c):
+            return False
+    return True
+
+def fix_equation(input_equation):
+    input_equation = input_equation.replace("^", "**")
+    input_equation = input_equation.replace("X", "x")
+    return input_equation
 
 def F(X):
     x = X
     return eval(equation)
 
 
+equation = input("Enter the Equation to be solved: ")
+equation = fix_equation(equation)
+print(equation)
+
+while input_validation(equation) == False:
+    equation = input("Invalid Syntax, Enter The equation again: ")
+    equation = fix_equation(equation)
+
 print("Enter the Minimum: ")
 l = int(input())
 print("Enter the Maximum: ")
-r = int(input())
+r = int(input())+1
 
-for i in range(l, r+1):
+validate_min_max(l, r)
+
+# print(equation)
+for i in range(l, r):
     Y = F(i)
     print("@ X = " + str(i) + " Y = " + str(Y));
-
-
-
 
